1562abb91fb1a6b6f742e18bff6ea93bfb127758
Rejina <rejinadangal4@gamil.com>
2025-06-17 17:00:10 +0545

    Dictionary in python

--- Python Basics/dictionary.py
+++ Python Basics/dictionary.py
@@ -14,8 +14,25 @@ print (dict1.values()) #printing values of dictionary
 print (dict1.items()) #printing key-value pairs of dictionary
 print (dict1["age"])
 
+
 for item in dict1.items():
     print(item) #printing items of dictionary
     
 for keys, values in dict1.items():
     print(f"{keys}: {values}") #printing items of dictionary
+    
+
+#updating in dictionary
+dict1["age"]=31
+
+#updating values with update function
+dict1.update({"age":31,"city":"Los Angeles"})
+
+#get function in dictionary
+print(dict1.get("name")) 
+
+#set default function
+dict.setdefault("Place:", "New York")
+
+print("Dictionary after updating", dict1)
+
